f89dd32479edd6fa3f185c50522b6585e4d377d8
hieuhuule <hieuhuule@gmail.com>
2022-08-02 17:40:46 -0500

    Hieu added his 2nd comment

--- pairing-cc-hl.js
+++ pairing-cc-hl.js
@@ -1,3 +1,6 @@
 Hello! This is a good start.
 
 Sup. This is a little hard. 
+Oof this is the second time i've tried this. the first time I made the changes to main.
+
+I pulled down your merged changes to my local -H 
