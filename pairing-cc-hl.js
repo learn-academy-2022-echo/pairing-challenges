98c3f457ce38ad440f6f73d6570a20b20cdc8025
hieuhuule <hieuhuule@gmail.com>
2022-08-02 17:51:24 -0500

    Hieu's 3rd comment

--- pairing-cc-hl.js
+++ pairing-cc-hl.js
@@ -3,6 +3,8 @@ Hello! This is a good start.
 Sup. This is a little hard. 
 Oof this is the second time i've tried this. the first time I made the changes to main.
 
-I pulled down your merged changes to my local -H 
+I pulled down your merged changes to my local -HL
 
 This is a great challenge. -CC
+
+What a headache this is. - HL
